b7f40981e12642850883c908a8db3f952ce95183
Dererk <dererk@debian.org>
2013-05-20 15:01:43 -0300

    Fix exit code

--- include/vcs_version.h
+++ include/vcs_version.h
@@ -1,10 +1,10 @@
-/* 2.1-64-g0fe0cba */
+/* 2.1-66-gb255d18-dirty */
 /*
  * NB:  This file is machine generated, DO NOT EDIT!
  *
  * Run make to regenerate
  *
  */
-/* 2.1-64-g0fe0cba */
+/* 2.1-66-gb255d18-dirty */
 
-#define VCS_Version "2.1-64-g0fe0cba"
+#define VCS_Version "2.1-66-gb255d18-dirty"

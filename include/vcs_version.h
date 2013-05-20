b255d187762b2068b36b04d93d9f03b6109baedc
Dererk <dererk@debian.org>
2013-05-20 13:20:15 -0300

    Add fixed include/vcs_version.h

---
+++ include/vcs_version.h
@@ -0,0 +1,10 @@
+/* 2.1-64-g0fe0cba */
+/*
+ * NB:  This file is machine generated, DO NOT EDIT!
+ *
+ * Run make to regenerate
+ *
+ */
+/* 2.1-64-g0fe0cba */
+
+#define VCS_Version "2.1-64-g0fe0cba"

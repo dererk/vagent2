79f9e9cb900977d24154e8d7b63a739ff7d3bb52
Dererk <dererk@debian.org>
2013-06-01 09:53:33 -0300

    Removed embedded jquery.js, default on quiet mode

--- include/vcs_version.h
+++ include/vcs_version.h
@@ -1,10 +1,10 @@
-/* 2.1-66-gb255d18-dirty */
+/* debian/2.2_git20130222-1-dirty */
 /*
  * NB:  This file is machine generated, DO NOT EDIT!
  *
  * Run make to regenerate
  *
  */
-/* 2.1-66-gb255d18-dirty */
+/* debian/2.2_git20130222-1-dirty */
 
-#define VCS_Version "2.1-66-gb255d18-dirty"
+#define VCS_Version "debian/2.2_git20130222-1-dirty"
